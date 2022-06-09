ca2cbd0aec8a945ea25dd9dd91954363a755d171
kwong1110 <kwong8535@naver.com>
2022-06-09 23:59:16 +0900

    Env - 페이네이션 쿼리 추가

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -24,3 +24,45 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
     },
   });
 };
+
+// post pagination
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+  const result = await graphql(
+    `
+      {
+        allMdx(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `,
+  );
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+  const posts = result.data.allMdx.edges;
+  const postsPerPage = 2;
+  const numPages = Math.ceil(posts.length / postsPerPage);
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      component: path.resolve('./src/templates/PostListTemplate.tsx'),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    });
+  });
+};
