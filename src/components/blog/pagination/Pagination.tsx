8f664d510a67c5f6bc8a0aa6d96c51071cae09cf
kwong1110 <kwong8535@naver.com>
2022-06-12 23:56:50 +0900

    Component - 동작(Pagination)

--- src/components/blog/pagination/Pagination.tsx
+++ src/components/blog/pagination/Pagination.tsx
@@ -1,26 +1,56 @@
-import { Link } from 'gatsby';
+import { Link, navigate } from 'gatsby';
 
 type PaginationProps = IPaginstion;
 
-const viewPageCount = 5;
+const pageRange = 5;
 
-const Pagination = ({ currentPage }: PaginationProps) => {
-  const pages = Array.from({ length: viewPageCount });
+const goPageLink = (pageNum: number) => {
+  if (pageNum !== 1) {
+    return `/blog/${pageNum}`;
+  }
+  return '/blog';
+};
+
+const Pagination = ({
+  currentPage,
+  numPages,
+  skip,
+  limit,
+}: PaginationProps) => {
+  const skipPages = Math.floor(skip / limit / pageRange) * pageRange;
+  const viewPageRange =
+    numPages < skipPages + pageRange ? numPages % pageRange : pageRange;
+
+  const pages = Array.from(
+    { length: viewPageRange },
+    (_v, index) => index + 1 + skipPages,
+  );
+
   return (
     <div>
-      {pages.map((_num, index) => (
+      <button
+        disabled={currentPage === 1}
+        style={{ padding: '0 10px', cursor: 'pointer' }}
+        onClick={() => navigate(goPageLink(currentPage - 1))}
+      >
+        뒤로
+      </button>
+      {pages.map(num => (
         <Link
-          key={index}
-          to={index === 0 ? '/blog' : `/blog/${index + 1}`}
-          style={
-            currentPage === index + 1
-              ? { border: '1px solid black' }
-              : undefined
-          }
+          key={num}
+          to={goPageLink(num)}
+          activeStyle={{ border: '1px solid black' }}
         >
-          {index + 1}
+          {num}
         </Link>
       ))}
+      <button
+        disabled={currentPage === numPages}
+        style={{ padding: '0 10px', cursor: 'pointer' }}
+        onClick={() => navigate(goPageLink(currentPage + 1))}
+      >
+        앞으로
+      </button>
     </div>
   );
 };
